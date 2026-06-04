e1d6776e227758aa1c062f722b4945ff5332b38b
Samir Azimov <samirazimov488@gmail.com>
2026-06-05 03:44:03 +0500

    added new endpoint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: '/checklist',
+  
+
   images: {
       domains: ['images.ctfassets.net', 'coin-images.coingecko.com'],
   }
