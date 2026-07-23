04c7450d6a177c44fd03ad28ed6a6f2a08894057
Samir Azimov <samirazimov488@gmail.com>
2026-07-23 19:19:24 +0500

    added new btn

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -5,14 +5,13 @@ import Image from "next/image";
 import RyanImg from "@/app/assets/ryan.jpg"
 import UserCard from "@/app/components/UserCard";
 
-
 const Profile = () => {
     return (
         <>
             <section className={'py-10 select-none '}>
                 <div className="container relative">
                     <div className="grid grid-cols-1 md:grid-cols-[70%_30%] gap-4 rounded-2xl ">
-                        <div className="bg-[#111] py-4 px-5 rounded-xl">
+                        <div className="bg-zinc-950 py-4 px-5 rounded-xl">
                             <span className={'text-white text-lg font-semibold'}>s.azimovs</span>
                             <div className="followers mt-3 mb-4 flex items-center gap-5">
                                 <div className={'flex items-center gap-2'}>
@@ -33,11 +32,7 @@ const Profile = () => {
                                 <UserCard />
                             </div>
 
-                            <div className={'flex items-center gap-2 absolute bottom-[-30]  '}>
-                                <span className={'text-sm text-gray-400 capitalize '}>Status</span>
-                                &mdash;
-                                <p className={'text-sm text-white lowercase'}>unavailable</p>
-                            </div>
+
                         </div>
                         <div className="photo w-full">
                             <Image
@@ -45,13 +40,14 @@ const Profile = () => {
                                 alt={'ryan'}
                                 width={'260'}
                                 height={'100'}
-
                                 className={'rounded-3xl  max-[425px]:w-full'}
                             />
                         </div>
                     </div>
                 </div>
             </section>
+
+
 
             <style jsx>
                 {`
@@ -68,9 +64,4 @@ const Profile = () => {
     )
 }
 
-
 export default Profile
-
-// 1923001453  AppID
-
-//ServerSecret 236fc79e937aaa30f14709e3b76c3a42
--- app/components/UserCard.tsx
+++ app/components/UserCard.tsx
@@ -10,18 +10,17 @@ const UserCard = () => {
     const roomID = [me,she].sort().join("_");
     return(
         <>
-            <div className={' flex items-center max-[425px]:w-full'}>
-                <button
-                    className={`max-[425px]:w-full
-                    border-none outline-none text-lg text-center text-white
-                    bg-linear-to-br from-sky-400 via-blue-600 to-violet-600 
-                    cursor-pointer
-                    py-2 px-5 rounded-xl  transition-transform duration-200 hover:-translate-y-1
+
+            <div className="fixed bottom-4 inset-x-0 mx-auto max-w-md px-4">
+                <div className="backdrop-blur-md bg-white/20 rounded-full shadow-lg p-3 flex justify-center items-center transition-all duration-300 hover:bg-white/50 cursor-pointer">
+                    <button
+                        className={`
                 `}
-                    onClick={() => router.push(`/call/${roomID}`)}
-                >
-                    Позвонить Раяну
-                </button>
+                        onClick={() => router.push(`/call/${roomID}`)}
+                    >
+                        Позвонить Раяну
+                    </button>
+                </div>
             </div>
 
         </>
