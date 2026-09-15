c81f865340d155bfbf89f5693eb4db318320b837
Samir Azimov <samirazimov488@gmail.com>
2026-09-15 23:39:16 +0500

    add pin-code component to the main pagex2

--- app/checklist/page.tsx
+++ app/checklist/page.tsx
@@ -1,6 +1,5 @@
 import BannerCoin from "@/app/components/shared/coins/bannerCoin";
 import CheckText from "../components/UI/CheckText";
-import ProtectedPage from "@/app/dashboard/page";
 
 
 const CheckList = () => {
@@ -9,7 +8,6 @@ const CheckList = () => {
             <main className={`bg-black min-h-screen overflow-hidden`}>
                 <CheckText />
                 <BannerCoin />
-                <ProtectedPage />
             </main>
         </>
     )
